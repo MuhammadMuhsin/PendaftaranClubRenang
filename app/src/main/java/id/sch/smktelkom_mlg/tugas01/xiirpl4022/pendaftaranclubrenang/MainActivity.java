67ed5abd9de9906e93f7973d1f2899ba09633400
MuhammadMuhsin <muhammad_muhsin23rpk@student.smktelkom-mlg.sch.id>
2016-09-21 16:59:29 +0700

    Menyediakan Widget yang diperlukan untuk kebutuhan dan menambahkan array untuk isi dari spinner.

---
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl4022/pendaftaranclubrenang/MainActivity.java
@@ -0,0 +1,22 @@
+package id.sch.smktelkom_mlg.tugas01.xiirpl4022.pendaftaranclubrenang;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity {
+
+}
+
+
