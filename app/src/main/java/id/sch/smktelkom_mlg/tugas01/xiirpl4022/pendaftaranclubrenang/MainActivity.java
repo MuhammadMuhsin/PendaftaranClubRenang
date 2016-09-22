5d4f50ad0cdba54d7f2c6c9667df7367a89e4982
MuhammadMuhsin <muhammad_muhsin23rpk@student.smktelkom-mlg.sch.id>
2016-09-22 09:18:45 +0700

    Mendeklarasikan parameter.

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl4022/pendaftaranclubrenang/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl4022/pendaftaranclubrenang/MainActivity.java
@@ -1,9 +1,7 @@
 package id.sch.smktelkom_mlg.tugas01.xiirpl4022.pendaftaranclubrenang;
 
-import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -11,12 +9,38 @@ import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
-
 public class MainActivity extends AppCompatActivity {
 
+    EditText etNama, etTgl, etBulan, etTahun, etAlamat;
+    RadioButton rbL, rbP;
+    CheckBox cbB, cbD, cbK, cbP;
+    Button bOk;
+    Spinner spK;
+    TextView tvHasil, tvHasil1, tvHasil2, tvHasil3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        spK = (Spinner) findViewById(R.id.spinner);
+        cbB = (CheckBox) findViewById(R.id.cbGB);
+        cbD = (CheckBox) findViewById(R.id.cbGD);
+        cbK = (CheckBox) findViewById(R.id.cbGK);
+        cbP = (CheckBox) findViewById(R.id.cbGP);
+        rbL = (RadioButton) findViewById(R.id.radioL);
+        rbP = (RadioButton) findViewById(R.id.radioP);
+        etNama = (EditText) findViewById(R.id.nama);
+        etTgl = (EditText) findViewById(R.id.tgl);
+        etBulan = (EditText) findViewById(R.id.bulan);
+        etTahun = (EditText) findViewById(R.id.tahun);
+        etAlamat = (EditText) findViewById(R.id.alamat);
+        bOk = (Button) findViewById(R.id.buttonOK);
+        tvHasil = (TextView) findViewById(R.id.hasil);
+        tvHasil1 = (TextView) findViewById(R.id.hasil1);
+        tvHasil2 = (TextView) findViewById(R.id.hasil2);
+        tvHasil3 = (TextView) findViewById(R.id.hasil3);
+    }
 }
 
 
